487912441668c41a017e67d61d512b776492979e
shanu-ck <shanu.dey@cloudkaptan.com>
2022-12-24 20:04:34 +0530

    implemented fetch cycles route
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.status(200).send("Welcome to Test Automation Backend");
+});
+
+app.post("/cycles", async (req, res) => {
+    const projectKey = req.body.project_key;
+    const apiToken = req.body.api_token;
+    try {
+        const response = await fetch(
+            `https://tcms.aiojiraapps.com/aio-tcms/api/v1/project/${projectKey}/testcycle`,
+            {
+                method: "GET",
+                headers: {
+                    Authorization: "AioAuth " + apiToken,
+                    "content-type": "application/json",
+                },
+            }
+        );
+        const resultJson = await response.json();
+        res.status(200).send(resultJson);
+    } catch (e) {
+        console.log(e);
+        return res.status(400).send({ error: "Failed to get cycles" });
+    }
 });
 
 module.exports = app;
